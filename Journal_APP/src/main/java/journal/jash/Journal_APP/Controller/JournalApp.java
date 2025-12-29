0455f758759e1b692be45d6e35ad37a6795853db
jashwanth2000 <jashwanth.bs@gmail.com>
2025-12-29 09:35:46 +0530

    Added CRUD operations

---
+++ Journal_APP/src/main/java/journal/jash/Journal_APP/Controller/JournalApp.java
@@ -0,0 +1,28 @@
+package journal.jash.Journal_APP.Controller;
+
+public class JournalApp {
+    
+private Integer id;
+private String notes;
+private String ref;
+public Integer getId() {
+    return id;
+}
+public void setId(Integer id) {
+    this.id = id;
+}
+public String getNotes() {
+    return notes;
+}
+public void setNotes(String notes) {
+    this.notes = notes;
+}
+public String getRef() {
+    return ref;
+}
+public void setRef(String ref) {
+    this.ref = ref;
+}
+
+
+}
---
+++ Journal_APP/src/main/java/journal/jash/Journal_APP/JournalAppApplication.java
@@ -0,0 +1,14 @@
+package journal.jash.Journal_APP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalAppApplication.class, args);
+		
+	}
+
+}
